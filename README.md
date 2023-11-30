2c08e532325f446ea5e8a24420fe74b0e279eca0
doubtfuldevil <soubusta@gmail.com>
2023-11-30 20:19:14 +0100

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# aoc2023
+My solutions for Advent of Code 2023 in Haskell
+
+https://adventofcode.com/2023
---
+++ _template.hs
@@ -0,0 +1,49 @@
+module AoC0X where
+
+import Data.List ( sort )
+import System.IO
+    ( openFile, hGetContents, Handle, IOMode(ReadMode) )
+
+---------------------------------------
+-- helper functions
+---------------------------------------
+getInput :: String -> String -> IO [String]
+getInput mode day = do
+    let filename = mode ++ day
+    h <- openFile filename ReadMode
+    content <- hGetContents h
+    return (lines content)
+
+safeTail :: [a] -> [a]
+safeTail [] = []
+safeTail (x:xs) = xs
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn _ [] = []
+splitOn del xs = takeWhile (/=del) xs:splitOn del (safeTail $ dropWhile (/=del) xs)
+---------------------------------------
+-- main functionality
+---------------------------------------
+type Input = [String]
+
+
+
+---------------------------------------
+-- start functions
+---------------------------------------
+day :: [Char]
+day = "0X"
+
+part1 :: IO ()
+part1 = do
+    testInput <- getInput "test" day
+    print (testInput)
+    --realInput <- getInput "input" day
+    --print(realInput)
+
+part2 :: IO ()
+part2 = do
+    testInput <- getInput "test" day
+    print (testInput)
+    --realInput <- getInput "input" day
+    --print(realInput)
